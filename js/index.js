43c9a535d232a3252a4211ad4b75d1eaa5156e05
Lucas Daniel <lucasfelepe@gmail.com>
2021-02-25 12:13:57 -0300

    implementado front-end completo de tela de login

---
+++ js/index.js
@@ -0,0 +1,19 @@
+function exibirPassword() {
+    const eye = document.getElementById('eye');
+    const eyeSlash = document.getElementById('eye-slash');
+    const fieldPassword = document.getElementById('campo-senha');
+
+    if (eye.style.display === 'none') {
+        eye.style.display = 'block';
+        eyeSlash.style.display = 'none';
+        fieldPassword.type = 'text';
+    } else {
+        eye.style.display = 'none';
+        eyeSlash.style.display = 'block';
+        fieldPassword.type = 'password';
+    }
+};
+
+document.getElementById('btn-login').addEventListener('click', function (e) {
+    e.preventDefault();
+});
